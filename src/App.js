9fdc7d2055bbd67ddd1082cf199a0d6a75eb086a
reflexLabs <64410700+reflexLabs@users.noreply.github.com>
2022-09-09 01:54:08 +0300

    LOL

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import {getData} from './Server';
+
 import Navbar from './components/navbar/Navbar';
 import Home from './components/Pages/Home';
 import About from './components/Pages/About';
@@ -10,16 +10,7 @@ import Contact from './components/Pages/Contact';
 import Gallery from './components/Pages/Gallery';
 import Footer from './components/footer/Footer';
 
-
-async function getDataFromServer(){
-  const res = await getData();
-  return res;
-}
-
 function App() {
-  getDataFromServer().then(function(result) {
-    console.log(result) // "Some User token"
- });
   return (
     <>
       <Navbar />
--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import {getData} from './Server';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+
+async function getDataFromServer(){
+  const res = await getData();
+  return res;
+}
+
+getDataFromServer().then(function(result) {
+  console.log(result) // "Some User token"
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
